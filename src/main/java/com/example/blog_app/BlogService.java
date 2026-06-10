2862d8f7e80862a5800bfb15d12e07b5b84695f8
irieda <2026033@fsc-educ.com>
2026-06-10 17:32:24 +0900

    今日やったとこ

--- src/main/java/com/example/blog_app/BlogService.java
+++ src/main/java/com/example/blog_app/BlogService.java
@@ -1,5 +1,21 @@
-package com.example.blog_app;
+package com.example.BlogService;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BlogService {
-    
+  private final BlogRepository blogRepository;
+
+  public BlogService(BlogRepository bookRepository) {
+    this.blogRepository = bookRepository;
+  }
+
+  public List<Blog> findAll() {
+    return blogRepository.findAll();
+  }
+
+  public void register(BlogForm form) {
+    blogRepository.save(new Blog(form.getTitle(), form.getPrice(), 0));
+  }
 }
